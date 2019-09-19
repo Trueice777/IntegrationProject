9ade1c6b6c40fbfc5b8d08cde853d4c35cc80109
Trueice777 <54412622+Trueice777@users.noreply.github.com>
2019-09-19 12:39:56 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,15 @@
 #Christopher Bryan
 #This is my trivia game.
+print("Welcome to my trivia game! Be prepared to be challenged!"
+correctTotal = 0
+answerOne = str(input("What is President Obama's last name:     A. Barack      B. Shirley      C. Obama        D. President"\n "Your Answer: "))
+if answerOne = C:
+      correctTotal += 1
+else:
+      pass
+answerTwo = str(input("
 
-print("hello")
-print(2-2)
+      
+
+      
+
