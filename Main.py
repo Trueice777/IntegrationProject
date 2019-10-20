8937feb14e430f42eef5fd8fad32753ac1ce73e8
Trueice777 <54412622+Trueice777@users.noreply.github.com>
2019-10-20 04:46:52 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,32 @@
-#Christopher Bryan
-#This is my trivia game.
-print("Welcome to my trivia game! Be prepared to be challenged!"
+# Christopher Bryan
+# This is my trivia game.
+
+
+def question(questionText, questionAnswer):
+    x = raw_input(questionText)
+    if x == questionAnswer:
+        return 1
+    else:
+        return 0
+
+
+print('Welcome to my trivia game! Be prepared to be challenged!')
+
 correctTotal = 0
-answerOne = str(input("What is President Obama's last name:     A. Barack      B. Shirley      C. Obama        D. President"\n "Your Answer: "))
-if answerOne = C:
-      correctTotal += 1
-else:
-      pass
-answerTwo = str(input("
+totalDone = 0
+
+questionList = ['The Statue of Liberty was given to the US by: (A. Britain B. Spain C. Canada D.France) ',
+                'How many have athletes have died in the modern olympics: (A. 5   B. 10   C. 30   D. 15) ']
+
+answerList = ['D', 'B']
+
+while totalDone < 2:
+    particularQuestion = questionList[totalDone]
+    particularAnswer = answerList[totalDone]
+    correctTotal += question(particularQuestion, particularAnswer)
+    totalDone += 1
+
+print('Your score: ', correctTotal, '/', totalDone)
 
       
 
