6e6554d77bc1d1ff04dbc161aeb40f8cdceff301
Trueice777 <54412622+Trueice777@users.noreply.github.com>
2019-10-20 12:28:54 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,20 +15,16 @@ print('Welcome to my trivia game! Be prepared to be challenged!')
 correctTotal = 0
 totalDone = 0
 
-questionList = ['The Statue of Liberty was given to the US by: (A. Britain B. Spain C. Canada D.France) ',
-                'How many have athletes have died in the modern olympics: (A. 5   B. 10   C. 30   D. 15) ']
+questionList = ['The Statue of Liberty was given to the US by: (A. Britain   B. Spain   C. Canada   D.France) ',
+                'How many have athletes have died in the modern olympics: (A. 5   B. 10   C. 30   D. 15) ',
+                'Which desert is largest: (A. Antarctica   B. Sahara   C. Gobi   D. Great Victoria) ',
+                'How many sides does a Toblerone have: (A. 23   B. 38   C. 41   D. 5) ',
+                'Which of these is not a berry: (A. Blueberry   B. Strawberry   C. Eggplant   D. Banana) ']
 
-answerList = ['D', 'B']
+answerList = ['D', 'B', 'A', 'B', 'B']
 
-while totalDone < 2:
-    particularQuestion = questionList[totalDone]
-    particularAnswer = answerList[totalDone]
-    correctTotal += question(particularQuestion, particularAnswer)
+while totalDone < 5:
+    correctTotal += question(questionList[totalDone], answerList[totalDone])
     totalDone += 1
 
 print('Your score: ', correctTotal, '/', totalDone)
-
-      
-
-      
-
